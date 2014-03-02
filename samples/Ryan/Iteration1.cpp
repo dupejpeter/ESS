809802213401a042ed51464fc74ed3e0e5c80d34
rnelson92 <theadmiral92@live.co.uk>
2014-03-02 16:35:14 +0000

    Update Iteration1.cpp

--- samples/Ryan/Iteration1.cpp
+++ samples/Ryan/Iteration1.cpp
@@ -7,9 +7,9 @@
 #include <unistd.h>
 #include <cmath>
 #include <iomanip>
+#include "C:\Users\Ryan\Documents\C++ Projects\ElectrostaticsA\Iteration1.h"
 
 using namespace std;
-
 // tolerance is the number of decimal places desired.
 
     void Iteration(Grid& A, int tolerance, int maxiterations)
@@ -43,7 +43,7 @@ using namespace std;
             	else if(A.fixed[i][j]==0 && (i == 0) && (j != 0) && (j != A.ncolumns-1))
             	{
             	    dold = A.potential[0][j];
-            		A.potential[0][j] = 0.25*(2*A.potential[1][j]+2*((A.potential[1][j]-A.potential[0][j]))+A.potential[0][j+1]+A.potential[0][j-1]);
+            		A.potential[0][j] = 0.25*(2*A.potential[1][j]+2*((A.potential[0][j]-A.potential[1][j]))+A.potential[0][j+1]+A.potential[0][j-1]);
             		dnew = A.potential[0][j];
             		if (derror < (abs (dnew-dold)))
                     {
@@ -54,7 +54,7 @@ using namespace std;
             	else if(A.fixed[i][j]==0 && (i==A.nrows-1) && (j!=A.ncolumns-1) && (j!=0))
             	{
             	    dold = A.potential[A.nrows-1][j];
-         		    A.potential[A.nrows-1][j] = 0.25*(2*A.potential[A.nrows-1][j]+2*((A.potential[A.nrows-2][j]-A.potential[A.nrows-1][j]))+A.potential[A.nrows-1][j+1]+A.potential[A.nrows-1][j-1]);
+         		    A.potential[A.nrows-1][j] = 0.25*(2*A.potential[A.nrows-2][j]+2*((A.potential[A.nrows-1][j]-A.potential[A.nrows-2][j]))+A.potential[A.nrows-1][j+1]+A.potential[A.nrows-1][j-1]);
          		    dnew = A.potential[A.nrows-1][j];
             		if (derror < abs (dnew-dold))
                     {
@@ -62,10 +62,10 @@ using namespace std;
                     }
             	}
 
-                else if(A.fixed[i][j]==0 && (j==0) && (i!=0) && (i!=A.nrows1))
+                else if(A.fixed[i][j]==0 && (j==0) && (i!=0) && (i!=A.nrows-1))
             	{
                     dold = A.potential[i][0];
-            		A.potential[i][0] = 0.25*(2*A.potential[i][1]+2*((A.potential[i][1]-A.potential[i][0]))+A.potential[i+1][0]+A.potential[i-1][0]);
+            		A.potential[i][0] = 0.25*(2*A.potential[i][1]+2*((A.potential[i][0]-A.potential[i][1]))+A.potential[i+1][0]+A.potential[i-1][0]);
             		dnew = A.potential[i][0];
             		if (derror < (abs (dnew-dold)))
                     {
@@ -76,7 +76,7 @@ using namespace std;
                 else if(A.fixed[i][j]==0 && (j==A.ncolumns-1) && (i!=0) && (i!=A.nrows-1))
             	{
             	    dold = A.potential[i][A.ncolumns-1];
-         		    potential[i][A.ncolumns-1] = 0.25*(2*A.potential[i][A.ncolumns-1]+2*((A.potential[i][A.ncolumns-2]-A.potential[i][A.ncolumns-1]))+A.potential[i+1][A.ncolumns-1]+A.potential[i-1][A.ncolumns-1]);
+         		    A.potential[i][A.ncolumns-1] = 0.25*(2*A.potential[i][A.ncolumns-2]+2*((A.potential[i][A.ncolumns-1]-A.potential[i][A.ncolumns-2]))+A.potential[i+1][A.ncolumns-1]+A.potential[i-1][A.ncolumns-1]);
          		    dnew = A.potential[i][A.ncolumns-1];
             		if (derror < abs (dnew-dold))
                     {
@@ -90,18 +90,19 @@ using namespace std;
                 else if(A.fixed[i][j]==0 && (i == 0) && (j == 0))
             	{
             	    dold = A.potential[i][j];
-            		A.potential[i][j] = 0.5*(A.potential[1][j]+((A.potential[1][j]-A.potential[0][j]))+A.potential[0][j+1]+((A.potential[i][1]-A.potential[i][0])));
+            		A.potential[i][j] = 0.5*(A.potential[1][0]+((A.potential[0][0]-A.potential[1][0]))+A.potential[0][1]+((A.potential[0][0]-A.potential[0][1])));
             		dnew = A.potential[i][j];
             		if (derror < (abs (dnew-dold)))
                     {
                         derror = abs (dnew-dold);
                     }
+                    cout << A.potential[i][j] << endl;
             	}
 
                 else if(A.fixed[i][j]==0 && (i == A.nrows-1) && (j == 0))
             	{
             	    dold = A.potential[i][j];
-            		A.potential[i][j] = 0.5*(A.potential[A.nrows-2][j]+((A.potential[A.nrows-2][j]-A.potential[A.nrows-1][j]))+A.potential[A.nrows-1][j+1]+((A.potential[A.nrows-1][j+1]-A.potential[A.nrows-1][j])));
+            		A.potential[i][j] = 0.5*(A.potential[A.nrows-2][j]+((A.potential[A.nrows-1][j]-A.potential[A.nrows-2][j]))+A.potential[A.nrows-1][j+1]+((A.potential[A.nrows-1][j]-A.potential[A.nrows-1][j+1])));
             		dnew = A.potential[i][j];
             		if (derror < (abs (dnew-dold)))
                     {
@@ -112,7 +113,7 @@ using namespace std;
                 else if(A.fixed[i][j]==0 && (i == 0) && (j == A.ncolumns-1))
             	{
             	    dold = A.potential[i][j];
-            		A.potential[i][j] = 0.5*(potential[i][A.ncolumns-2]+((A.potential[i][A.ncolumns-2]-A.potential[i][A.ncolumns-1]))+A.potential[i+1][A.ncolumns-1]+((A.potential[i+1][A.ncolumns-1]-A.potential[i][A.ncolumns-1])));
+            		A.potential[i][j] = 0.5*(A.potential[i][A.ncolumns-2]+((A.potential[i][A.ncolumns-1]-A.potential[i][A.ncolumns-2]))+A.potential[i+1][A.ncolumns-1]+((A.potential[i][A.ncolumns-1]-A.potential[i+1][A.ncolumns-1])));
             		dnew = A.potential[i][j];
             		if (derror < (abs (dnew-dold)))
                     {
@@ -123,7 +124,7 @@ using namespace std;
                 else if(A.fixed[i][j]==0 && (i == A.nrows-1) && (j == A.ncolumns-1))
             	{
             	    dold = A.potential[i][j];
-            		A.potential[i][j] = 0.5*(A.potential[A.nrows-2][j]+((A.potential[A.nrows-2][j]-A.potential[A.nrows-1][j]))+A.potential[i][A.ncolumns-2]+((A.potential[i][A.ncolumns-2]-A.potential[i][A.ncolumns-1])));
+            		A.potential[i][j] = 0.5*(A.potential[A.nrows-2][j]+((A.potential[A.nrows-1][j]-A.potential[A.nrows-2][j]))+A.potential[i][A.ncolumns-2]+((A.potential[i][A.ncolumns-1]-A.potential[i][A.ncolumns-2])));
             		dnew = A.potential[i][j];
             		if (derror < (abs (dnew-dold)))
                     {
