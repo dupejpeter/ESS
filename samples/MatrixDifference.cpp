f1fff7e8a20cf0467684e3a442c7e014a3abdfd2
Linkyn <michelbiersbach@hotmail.com>
2014-03-04 08:14:23 +0000

    Update MatrixDifference.cpp
    Modified to determine percentage difference between two matrices instead of straight difference.

--- samples/MatrixDifference.cpp
+++ samples/MatrixDifference.cpp
@@ -1,6 +1,9 @@
-#include "stdafx.h"
+/*#include <stdafx.h> - use when working with Visual Studio*/
 #include <iostream>
 #include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+
 
 
 using namespace std;
@@ -9,50 +12,58 @@ using namespace std;
 class matrix
 {
 public:
-    int i, j, nRank;
-    double adArray[100][100];
+	int i, j, nRank;
+	double adArray[100][100];
 
-    matrix operator-(matrix m1);
-    void input();
-    void output();
-    void maximum(matrix m1);
+	matrix operator-(matrix m1);
+	void input();
+	void output();
+	void maximum(matrix m1);
 };
 
 void matrix::input()
 {
-    cout << "\nEnter rank: ";
-    cin >> nRank;
-    cout << "\nEnter the elements:\n\n";
-    for(i = 0; i < nRank; i++)
-    {
-        for(j = 0; j < nRank; j++)
-        {
-            cin >> adArray[i][j];
-            cout << "\t";
-        }
-        cout << "\n";
-    }
+	cout << "\nEnter rank: ";
+	cin >> nRank;
+	cout << "\nEnter the elements:\n\n";
+	for (i = 0; i < nRank; i++)
+	{
+		for (j = 0; j < nRank; j++)
+		{
+			cin >> adArray[i][j];
+			cout << "\t";
+		}
+		cout << "\n";
+	}
 }
 matrix matrix::operator-(matrix m1)
 {
-    matrix temp;
-    temp.nRank = nRank;
+	matrix temp;
+	temp.nRank = nRank;
 
-    for(i = 0; i < temp.nRank; i++)
-        for(j = 0; j < temp.nRank; j++)
-            temp.adArray[i][j] = adArray[i][j] - m1.adArray[i][j];
-return temp;
+	for (i = 0; i < temp.nRank; i++)
+	for (j = 0; j < temp.nRank; j++)
+	
+	if (adArray[i][j] != 0) // Fraction is only defined when denominator is non-zero.
+	
+		temp.adArray[i][j] = (m1.adArray[i][j] - adArray[i][j]) / abs(adArray[i][j])*100;
+		
+	else // Use zero as placeholder as infinite increase / decrease cannot be used for proper comparison, anyway.
+
+		temp.adArray[i][j] = 0;
+
+	return temp;
 }
 
 void matrix::output()
 {
-    cout << "Matrix is:\n\n";
-    for(i = 0; i < nRank; i++)
-    {
-        for(j = 0;j < nRank; j++)
-            cout << adArray[i][j]<<"\t";
-        cout << "\n";
-    }
+	cout << "Matrix is:\n\n";
+	for (i = 0; i < nRank; i++)
+	{
+		for (j = 0; j < nRank; j++)
+			cout << adArray[i][j] << "\t";
+		cout << "\n";
+	}
 
 }
 
@@ -70,24 +81,24 @@ void matrix::maximum(matrix m1)
 
 	}
 
-	cout << "The maximum value is attained for element " << index1+1 << "-" <<
-	index2+1 << ", corresponding to a value of " << m1.adArray[index1][index2] <<".\n";
+	cout << "The maximum value is attained for element " << index1 + 1 << "-" <<
+		index2 + 1 << ", corresponding to a value of " << m1.adArray[index1][index2] << ".\n";
 }
 
 int main()
 {
-    matrix m1, m2, m3;
-    
-    m1.input();
-    m2.input();
-    
-    m3 = m1 - m2;
-    
-    m3.output();
-    m3.maximum(m3);
-    
-    getch();
-    
-    return 0;
+	matrix m1, m2, m3;
+
+	m1.input();
+	m2.input();
+
+	m3 = m1 - m2;
+
+	m3.output();
+	m3.maximum(m3);
+
+	getch(); //_getch when using Visual Studio
+
+	return 0;
 
 }
