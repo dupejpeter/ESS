e556ec8e1fce0e7db099dde36089182ba3419295
Peter Dupej <dupejpeter@gmail.com>
2014-03-12 00:26:11 +0000

    Forgotten naming convention changed

--- src/Grid_test.cpp
+++ src/Grid_test.cpp
@@ -14,16 +14,16 @@ using namespace std;
 int main() {
 	try {
 		Grid g = Grid(1, 1);
-		cout << "g.getSizeX() = " << g.getSizeX() << endl;
-		cout << "g.getSizeY() = " << g.getSizeY() << endl;
-		cout << "g.getPot(0, 0) = " << g.getPot(0, 0) << endl;
-		g.setPot(0, 0, 9.999999);
+		cout << "g.getSizeX() = " << g.GetSizeX() << endl;
+		cout << "g.getSizeY() = " << g.GetSizeY() << endl;
+		cout << "g.getPot(0, 0) = " << g.GetPot(0, 0) << endl;
+		g.SetPot(0, 0, 9.999999);
 		cout << "g.setPot(0, 0, 9.999999)" << endl;
-		cout << "g.getPot(0, 0) = " << g.getPot(0, 0) << endl;
-		cout << "g.isFixed(0, 0) = " << g.isFixed(0, 0) << endl;
-		g.setFixed(0, 0, 1);
+		cout << "g.getPot(0, 0) = " << g.GetPot(0, 0) << endl;
+		cout << "g.isFixed(0, 0) = " << g.IsFixed(0, 0) << endl;
+		g.SetFixed(0, 0, 1);
 		cout << "g.setFixed(0, 0, 1)" << endl;
-		cout << "g.isFixed(0, 0) = " << g.isFixed(0, 0) << endl;
+		cout << "g.isFixed(0, 0) = " << g.IsFixed(0, 0) << endl;
 	} catch (invalid_argument &e) {
 		cerr << "Invalid argument: " << e.what() << endl;
 	} catch (out_of_range &e) {
